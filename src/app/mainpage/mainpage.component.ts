833e03278d0a6dd566d497d00e68183091106d41
Dmitriy Tuchashvili <pechorin211@gmail.com>
2019-10-19 10:00:52 +0600

    style

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {DataPassService} from '../shared/datapass.service';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-mainpage',
@@ -12,7 +13,7 @@ export class MainpageComponent implements OnInit {
   user: string;
 
 
-  constructor(private datapassservice: DataPassService) {
+  constructor(private datapassservice: DataPassService, private router: Router) {
     this.datapassservice.currentUser$.subscribe((data) => {
       this.user = data;
       if (this.user !== 'anonymous') {
@@ -29,5 +30,13 @@ export class MainpageComponent implements OnInit {
     this.datapassservice.user.next('anonymous');
     this.user = 'anonymous';
     this.logged = false;
+  }
+
+  startNow() {
+    if (this.logged) {
+      this.router.navigateByUrl('categories');
+    } else {
+      this.router.navigateByUrl('auth/login');
+    }
   }
 }
