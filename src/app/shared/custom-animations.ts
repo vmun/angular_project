5b2e1cdd969f10f760d6982499e2bec99c6984a7
vladek1934 <vlad.mun1999@gmail.com>
2019-11-16 10:40:28 +0600

    chart added

--- src/app/shared/custom-animations.ts
+++ src/app/shared/custom-animations.ts
@@ -12,18 +12,18 @@ export const customAnimations =
   trigger('customAnimations', [
     transition('* => *', [
       query(':enter, :leave',
-        style({ position: 'fixed', width: '100%' }),
+        style({ position: 'fixed', width: '60%' }),
         { optional: true }),
       group([
         query(':enter', [
-          style({ transform: 'translateX(140%)' }),
+          style({ transform: 'translateX(100%)' }),
           animate('1s ease-in-out',
             style({ transform: 'translateX(0%)' }))
         ], { optional: true }),
         query(':leave', [
           style({ transform:   'translateX(0%)'}),
           animate('1s ease-in-out',
-            style({ transform: 'translateX(-140%)' }))
+            style({ transform: 'translateX(-100%)' }))
         ], { optional: true }),
       ])
     ])
