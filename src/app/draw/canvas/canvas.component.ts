b7513bf3eb55dbe721d071ab2bdf9849be89d734
Dmitriy Tuchashvili <pechorin211@gmail.com>
2019-11-29 12:54:01 +0600

    changes

--- src/app/draw/canvas/canvas.component.ts
+++ src/app/draw/canvas/canvas.component.ts
@@ -9,26 +9,102 @@ import {DataPassService} from '../../shared/datapass.service';
 })
 export class CanvasComponent implements OnInit {
 
-  imageElement;
   currentImage = 0;
+
+  imageElement: HTMLImageElement;
+  canvasElement: HTMLCanvasElement;
+  canvasCtx;
+
+  pointsBuffer = [];
+  polygonsBuffer = [];
 
   constructor(private tempData: TempDataService, private datapassservice: DataPassService) {
     this.datapassservice.currentImage$.subscribe((data) => {
       if (this.imageElement) {
         this.currentImage = data;
-        console.log(this.currentImage);
         this.changeImage(data);
       }
     });
   }
 
   ngOnInit() {
-    this.imageElement = document.getElementById('image');
+    this.imageElement = document.getElementById('image') as HTMLImageElement;
+    this.canvasElement = document.getElementById('canvas') as HTMLCanvasElement;
+    this.canvasCtx = this.canvasElement.getContext('2d');
     this.changeImage(0);
+    this.imageElement.onload = () => {
+      this.canvasElement.style.top = this.imageElement.clientTop.toString();
+      this.canvasElement.style.left = this.imageElement.clientLeft.toString();
+      this.canvasElement.height = this.imageElement.clientHeight;
+      this.canvasElement.width = this.imageElement.clientWidth;
+      this.canvasCtx.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
+    };
   }
 
   changeImage(index) {
     this.imageElement.setAttribute( 'src', this.tempData.imagesFull.find(im => im.id === index).url);
+    this.polygonsBuffer = [];
+    this.pointsBuffer = [];
   }
 
+
+
+  canvasClick(event) {
+    const point = {x: event.offsetX, y: event.offsetY};
+    this.drawPoint(point, 'rgba(255, 0, 0, 0.5)');
+
+    if (this.pointsBuffer.length > 0) {
+      this.drawLine(point, 'rgba(255, 0, 0, 0.5)');
+    }
+
+    if ((this.pointsBuffer.length >= 3) && this.isNear(point, this.pointsBuffer[0])) {
+      this.polygonsBuffer.push(this.pointsBuffer);
+      this.drawPolygons();
+      this.pointsBuffer = [];
+    } else {
+      this.pointsBuffer.push(point);
+    }
+  }
+
+  drawPoint(point, color) {
+    this.canvasCtx.beginPath();
+    this.canvasCtx.arc(point.x, point.y, 6, 0, 2 * Math.PI);
+    this.canvasCtx.fillStyle = color;
+    this.canvasCtx.fill();
+    this.canvasCtx.lineWidth = 1;
+    this.canvasCtx.strokeStyle = 'black';
+    this.canvasCtx.stroke();
+  }
+
+  drawLine(point, color) {
+    const prevPoint = this.pointsBuffer[this.pointsBuffer.length - 1];
+    this.canvasCtx.beginPath();
+    this.canvasCtx.moveTo(prevPoint.x, prevPoint.y);
+    this.canvasCtx.lineTo(point.x, point.y);
+    this.canvasCtx.lineWidth = 4;
+    this.canvasCtx.strokeStyle = color;
+    this.canvasCtx.stroke();
+  }
+
+  drawPolygons() {
+    this.canvasCtx.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
+    this.canvasCtx.fillStyle = 'rgba(255, 0, 0, 0.5)';
+    this.canvasCtx.beginPath();
+    this.polygonsBuffer.forEach((points) => {
+      points.forEach((point, index) => {
+        if (index === 0) {
+          this.canvasCtx.moveTo(point.x, point.y);
+        } else {
+          this.canvasCtx.lineTo(points[index].x, points[index].y);
+        }
+      });
+    });
+    this.canvasCtx.closePath();
+    this.canvasCtx.fill();
+  }
+
+  isNear(point1, point2) {
+    return (point1.x >= point2.x - 5 && point1.x <= point2.x + 5)
+      && (point1.y >= point2.y - 5 && point1.y <= point2.y + 5);
+  }
 }
